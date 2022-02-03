fd1656e945d17cf64dfc789d15d4c075727373be
Ajay Lotan Thakur <thakur.ajay@gmail.com>
2022-02-03 13:10:50 -0800

    License cleanup (#4)

--- api_mongoDB.go
+++ api_mongoDB.go
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
+// Copyright 2019 free5GC.org
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
 package MongoDBLibrary
 
 import (
